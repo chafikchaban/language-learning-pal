1046a38919026e42158fa37efc1d692a67549d23
Chafik <chafikchaban@gmail.com>
2024-05-15 18:13:54 +0200

    add profile page and update home page (#6)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,17 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
     path: 'home',
     loadComponent: () => import('./screens/home/home.page').then((m) => m.HomePage),
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    path: 'languages',
+    loadComponent: () => import('./screens/languages/languages.page').then(m => m.LanguagesPage)
   },
   {
     title: 'levels',
@@ -18,11 +22,16 @@ export const routes: Routes = [
   {
     title: 'steps',
     path: 'level/:languageCode/:id',
-    loadComponent: () => import('./screens/steps/steps.page').then( m => m.StepsPage)
+    loadComponent: () => import('./screens/steps/steps.page').then(m => m.StepsPage)
   },
   {
     title: 'slideshows',
     path: 'step/:languageCode/:levelId/:id',
-    loadComponent: () => import('./screens/slideshows/slideshows.page').then( m => m.SlideshowsPage)
+    loadComponent: () => import('./screens/slideshows/slideshows.page').then(m => m.SlideshowsPage)
   },
+  {
+    path: 'profile',
+    loadComponent: () => import('./screens/profile/profile.page').then(m => m.ProfilePage)
+  },
+
 ];
---
+++ src/app/screens/languages/languages.page.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+import { Component, inject } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonButton } from '@ionic/angular/standalone';
+import { DataService } from 'src/app/services/data/data.service';
+import { Language } from 'src/app/model/language.model';
+
+@Component({
+  selector: 'app-languages',
+  templateUrl: './languages.page.html',
+  styleUrls: ['./languages.page.scss'],
+  standalone: true,
+  imports: [CommonModule, RouterLink, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonButton],
+})
+export class LanguagesPage {
+
+  private dataService = inject(DataService);
+
+  public selectedLanguage?: string;
+
+  public languages: Language[] = []
+
+  constructor() {
+    this.dataService.getLanguages().subscribe(data => {
+      this.languages = data.languages
+    });
+  }
+
+  public select = (languageCode: string): void => {
+    this.selectedLanguage = languageCode
+  }
+}
